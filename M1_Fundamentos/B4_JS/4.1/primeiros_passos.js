e38de94e444895dabd5039383b2cb48d3bfb4df2
Leo Araujo <leonardo_02452@hotmail.com>
2021-11-26 20:04:20 -0300

    Feito até o ex. 4

---
+++ M1_Fundamentos/B4_JS/4.1/primeiros_passos.js
@@ -0,0 +1,37 @@
+// Exercicio 1
+const a = 10;
+const b = 7;
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a / b);
+console.log (a % b);
+
+//Exericio 2
+if (a > b){
+    console.log(a);
+} else {
+    console(b);
+}
+
+//Exericio 3
+const c = 12;
+
+if (a > b && a > c){
+    console.log(a);
+} else if (b > a && b > c) {
+    console(b);
+} else if (c > a && c > b) {
+    console.log(c);
+}
+
+//Exercicio 4
+var numero = 0;
+
+if (numero > 0) {
+    console.log("positive");
+} else if (numero < 0) {
+    console.log("negative");
+} else if (numero === 0) {
+    console.log("zero")
+}
