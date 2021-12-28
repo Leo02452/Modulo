dde786ed9e30e4ce9879d63157df7a6125dc224f
Leo Araujo <leonardo_02452@hotmail.com>
2021-12-28 19:55:20 -0300

    Conteudo pt1 concluído

--- M1_Fundamentos/B5_DOM-eventos-webStorage/5.2/script.js
+++ M1_Fundamentos/B5_DOM-eventos-webStorage/5.2/script.js
@@ -1,5 +1,25 @@
-document.getElementById('elementoOndeVoceEsta').parentElement.style.color = "blue";
+// 1. Acesse o elemento elementoOndeVoceEsta
+const ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
-document.querySelector('#primeiroFilhoDoFilho').innerText = "Colocando um texto";
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+ondeVoceEsta.parentNode.style.color = 'blue';
 
-document.getElementById('primeiroFilho').parentNode.style.backgroundColor = "black";
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+primeiroFilhoDoFilho.innerText = 'Adicionando um texto';
+
+// 4. Acesse o primeiroFilho a partir de pai
+const pai = document.getElementById('pai');
+let primeiroFilho = pai.firstElementChild;
+
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta
+primeiroFilho = ondeVoceEsta.previousElementSibling;
+
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta
+const textAtencao = ondeVoceEsta.nextSibling;
+
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta
+let terceiroFilho = ondeVoceEsta.nextElementSibling;
+
+// 8. Agora acesse o terceiroFilho a partir de pai
+terceiroFilho = pai.children[2];
