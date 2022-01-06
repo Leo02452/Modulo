827f77f77707d8d0fc14d40398fda1a44545d937
Leo Araujo <leonardo_02452@hotmail.com>
2022-01-06 14:48:30 -0300

    atualizacao de arquivos

--- M1_Fundamentos/B6_Forms_Flexbox_Responsivo/6.2/script.js
+++ M1_Fundamentos/B6_Forms_Flexbox_Responsivo/6.2/script.js
@@ -45,3 +45,27 @@ campoData.DatePickerX.init({
 });
 
 let dataEscolhida = campoData.value;
+
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
