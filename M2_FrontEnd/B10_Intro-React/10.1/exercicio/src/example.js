14c65441990ddbbf578ed886ba8028e444f3fb68
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-16 18:25:16 -0300

    Update README

---
+++ M2_FrontEnd/B10_Intro-React/10.1/exercicio/src/example.js
@@ -0,0 +1,9 @@
+const Task = (value) => {
+  return value
+}
+
+const tasksArray = ['Acordar', 'Tomar café', 'Estudar', 'Almoçar', 'Ver vídeos'];
+
+// tasksArray.map((task) => console.log(task));
+
+console.log(Task(tasksArray));
